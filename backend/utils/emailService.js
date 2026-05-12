f2dd53fd9834a2f749a68c88683cc7f9f574454e
Tharasri <tharasri06@gmail.com>
2026-05-12 19:53:40 +0530

    mail issues

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -2,8 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   family: 4,
   auth: {
     user: process.env.EMAIL_USER,
